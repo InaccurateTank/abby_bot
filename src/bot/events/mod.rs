cdea87d185b9ede0a45c3a8920fdbbd9ffeea9f1
InaccurateTank <inaccuratetank@outlook.com>
2023-03-19 19:19:15 -0700

    Moved event handler

--- src/bot/events/mod.rs
+++ src/bot/events/mod.rs
@@ -1,2 +1,77 @@
-pub mod message;
-pub mod interaction;
+use poise::serenity_prelude as serenity;
+use crate::{db_structs,Error, Data};
+use sqlx::{query, query_as};
+
+mod message;
+mod interaction;
+
+pub async fn event_handler<'a>(ctx: &serenity::Context, event: &poise::Event<'a>, _framework: poise::FrameworkContext<'a, Data, Error>, data: &Data) -> Result<(), Error> {
+	match event {
+		// Join Server
+		poise::Event::GuildCreate { guild, is_new } => {
+			if *is_new {
+				let id = guild.id.as_u64();
+				println!("Creating database entry for server {id}.");
+				query("INSERT INTO servers (srvid) VALUES(?);")
+					.bind(*id as i64)
+					.execute(&data.db)
+					.await?;
+				guild.system_channel_id.unwrap().send_message(ctx, |m| {
+					m.content("");
+					m.embed(|e|{
+						e.title("Hello I'm Abby!");
+						e.color(serenity::utils::Color::from_rgb(102, 51, 102));
+						e.description("I am general purpose discord bot. To start using my local features on this server, please have an admin run `/setup bot`. For other global commands type /help.");
+						e.field("Disclosure", format!("I operate off a database to keep track of settings between servers and reboots. The database consists entirely of booleans and numerical IDs with zero user information or identifying data. If this still concerns you, you can browse the entire implementation at my repository [here]({}).", env!("CARGO_PKG_REPOSITORY")), true)
+					})
+				}).await?;
+			}
+		},
+
+		// Kicked from Server
+		poise::Event::GuildDelete { incomplete, full: _ } => {
+			let id = incomplete.id.as_u64();
+			println!("Deleting server {id} from database.");
+			query("DELETE FROM servers WHERE srvid = ?;")
+				.bind(*id as i64)
+				.execute(&data.db)
+				.await?;
+			query("DELETE FROM role_options WHERE srvid = ?;")
+				.bind(*id as i64)
+				.execute(&data.db)
+				.await?;
+			query(&format!("DROP TABLE IF EXISTS roles_{id};"))
+				.execute(&data.db)
+				.await?;
+		},
+
+		// On Login
+		poise::Event::Ready { data_about_bot } => {
+			println!("{} is connected!", data_about_bot.user.name);
+			ctx.set_activity(serenity::Activity::watching("Everything")).await;
+		},
+
+		// On Message in Channel
+		poise::Event::Message { new_message } => {
+			let srv_features = match new_message.guild_id {
+				Some(id) => Some(query_as::<_, db_structs::Server>("SELECT * FROM servers WHERE srvid = ?;")
+					.bind(*id.as_u64() as i64)
+					.fetch_one(&data.db)
+					.await?),
+				None => None
+			}.unwrap_or_default();
+			if !new_message.is_own(&ctx.cache) && srv_features.messages {
+				message::handler(ctx, new_message, srv_features).await?;
+			}
+		},
+
+		// Longform Message Interactions
+		poise::Event::InteractionCreate { interaction: serenity::Interaction::MessageComponent(m) } => {
+			interaction::mci_handler(ctx, data, m).await?;
+		}
+
+		// Otherwise Nothing
+		_ => ()
+	}
+	Ok(())
+}
--- src/bot/main.rs
+++ src/bot/main.rs
@@ -3,8 +3,7 @@ use gumdrop::Options;
 use sqlx::{
 	migrate::MigrateDatabase,
 	Sqlite,
-	sqlite::SqlitePoolOptions,
-	query, query_as
+	sqlite::SqlitePoolOptions
 };
 use abby_utils::{Context, Error, Data, Config, db_structs};
 
@@ -71,78 +70,7 @@ async fn main() -> Result<(), Error> {
 			edit_tracker: Some(poise::EditTracker::for_timespan(std::time::Duration::from_secs(3600))),
 			..Default::default()
 		},
-		event_handler: |ctx, event, _framework, data| {
-			Box::pin(async move {
-				match event {
-					// Join Server
-					poise::Event::GuildCreate { guild, is_new } => {
-						if *is_new {
-							let id = guild.id.as_u64();
-							println!("Creating database entry for server {id}.");
-							query("INSERT INTO servers (srvid) VALUES(?);")
-								.bind(*id as i64)
-								.execute(&data.db)
-								.await?;
-							guild.system_channel_id.unwrap().send_message(ctx, |m| {
-								m.content("");
-								m.embed(|e|{
-									e.title("Hello I'm Abby!");
-									e.color(serenity::utils::Color::from_rgb(102, 51, 102));
-									e.description("I am general purpose discord bot. To start using my local features on this server, please have an admin run `/setup bot`. For other global commands type /help.");
-									e.field("Disclosure", format!("I operate off a database to keep track of settings between servers and reboots. The database consists entirely of booleans and numerical IDs with zero user information or identifying data. If this still concerns you, you can browse the entire implementation at my repository [here]({}).", env!("CARGO_PKG_REPOSITORY")), true)
-								})
-							}).await?;
-						}
-					},
-
-					// Kicked from Server
-					poise::Event::GuildDelete { incomplete, full: _ } => {
-						let id = incomplete.id.as_u64();
-						println!("Deleting server {id} from database.");
-						query("DELETE FROM servers WHERE srvid = ?;")
-							.bind(*id as i64)
-							.execute(&data.db)
-							.await?;
-						query("DELETE FROM role_options WHERE srvid = ?;")
-							.bind(*id as i64)
-							.execute(&data.db)
-							.await?;
-						query(&format!("DROP TABLE IF EXISTS roles_{id};"))
-							.execute(&data.db)
-							.await?;
-					},
-
-					// On Login
-					poise::Event::Ready { data_about_bot } => {
-						println!("{} is connected!", data_about_bot.user.name);
-						ctx.set_activity(serenity::Activity::watching("Everything")).await;
-					},
-
-					// On Message in Channel
-					poise::Event::Message { new_message } => {
-						let srv_features = match new_message.guild_id {
-							Some(id) => Some(query_as::<_, db_structs::Server>("SELECT * FROM servers WHERE srvid = ?;")
-								.bind(*id.as_u64() as i64)
-								.fetch_one(&data.db)
-								.await?),
-							None => None
-						}.unwrap_or_default();
-						if !new_message.is_own(&ctx.cache) && srv_features.messages {
-							events::message::handler(ctx, new_message, srv_features).await?;
-						}
-					},
-
-					// Longform Message Interactions
-					poise::Event::InteractionCreate { interaction: serenity::Interaction::MessageComponent(m) } => {
-						events::interaction::mci_handler(ctx, data, m).await?;
-					}
-
-					// Otherwise Nothing
-					_ => ()
-				}
-				Ok(())
-			})
-		},
+		event_handler: |ctx, event, framework, data| Box::pin(events::event_handler(ctx, event, framework, data)),
 		..Default::default()
 	};
 	let intents = serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::GUILD_MESSAGES | serenity::GatewayIntents::MESSAGE_CONTENT;
