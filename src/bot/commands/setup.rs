bc683220b4ee23166ae3fbc3368b6ae302dda24e
InaccurateTank <inaccuratetank@outlook.com>
2023-04-05 21:18:59 -0700

    mass delete works again

--- src/bot/commands/setup.rs
+++ src/bot/commands/setup.rs
@@ -179,13 +179,27 @@ async fn bot(ctx: Context<'_>) -> Result<(), Error> {
 					.to_guild_cached(ctx)
 					.unwrap()
 					.system_channel_id
-					.unwrap()
+					.unwrap_or(ctx.guild()
+						.unwrap()
+						.default_channel(ctx.framework().bot_id)
+						.await
+						.unwrap()
+						.id)
 					.as_u64() as i64
 				);
 			for grp in query_as::<_, db_structs::RoleGroup>(&format!("SELECT * FROM rgroups_{srv_id}"))
 				.fetch_all(&ctx.data().db)
 				.await? {
-				ctx.http().delete_message(group_channel as u64, grp.msg as u64).await?;
+				if let Err(_) = ctx.http().delete_message(group_channel as u64, grp.msg as u64).await {
+					ctx.send(|m| {
+						m.content("");
+						m.ephemeral(true);
+						m.embed(|e| {
+							templates::builder_state_embed(e, false, &format!("Either can't find or can't delete the message for the role group \"{}\". Entries will be removed from the database, but the message will need to be deleted manually.", grp.name));
+							e
+						})
+					}).await?;
+				}
 			}
 
 			// Purge Database
