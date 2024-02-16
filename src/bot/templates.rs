e809c255e667f36ba5f7b65c46245fb255932a69
InaccurateTank <inaccuratetank@outlook.com>
2024-02-16 15:35:30 -0800

    purging old utils

--- src/bot/templates.rs
+++ src/bot/templates.rs
@@ -1,9 +1,10 @@
 use poise::serenity_prelude as serenity;
 use serenity::{ CreateActionRow, CreateButton };
-use crate::utils::concat;
-use crate::structs::db;
-// use abby_utils::{db_structs, concat};
-use crate::{EMBED_STD, EMBED_WAIT, EMBED_FAIL};
+use crate::{
+	structs::db,
+	utils,
+	EMBED_STD, EMBED_WAIT, EMBED_FAIL
+};
 
 /// Makes a [`serenity::CreateEmbed`] and fills it out using the builder function.
 pub fn state_embed(success: bool, text: &str) -> serenity::CreateEmbed {
@@ -18,30 +19,12 @@ pub fn state_embed(success: bool, text: &str) -> serenity::CreateEmbed {
 		)
 }
 
-/// Sets the [`serenity::CreateEmbed`] with the state_embed values.
-// pub fn builder_state_embed(e: &mut serenity::CreateEmbed, success: bool, text: &str) {
-// 	if success {
-// 		e.title(":white_check_mark: Success :white_check_mark:");
-// 		e.color(EMBED_STD);
-// 	} else {
-// 		e.title(":warning: Failure :warning:");
-// 		e.color(EMBED_FAIL);
-// 	}
-// 	e.description(text);
-// }
-
 /// Makes a [`serenity::CreateEmbed`] and fills it out using the builder function.
 pub fn processing_embed() -> serenity::CreateEmbed {
 	serenity::CreateEmbed::new()
 		.color(EMBED_WAIT)
 		.description("Processing, please wait...")
 }
-
-// Sets the [`serenity::CreateEmbed`] with the processing_embed values.
-// pub fn builder_processing_embed(e: &mut serenity::CreateEmbed) {
-// 	e.color(EMBED_WAIT);
-// 	e.description("Processing, please wait...");
-// }
 
 pub fn rolelist_embed(ctx: &serenity::Context, group: &str, rolelist: Vec<db::RoleEntry>) -> serenity::CreateEmbed {
 	let mut rolelist: Vec<(String, u16)> = rolelist.into_iter().map(|r|
@@ -55,8 +38,8 @@ pub fn rolelist_embed(ctx: &serenity::Context, group: &str, rolelist: Vec<db::Ro
 	let mut name_str = String::new();
 	let mut user_str = String::new();
 	for (name, id) in rolelist {
-		name_str = concat(&name_str, &format!("{name}\n"));
-		user_str = concat(&user_str, &format!("{id}\n"));
+		name_str = utils::concat(&name_str, &format!("{name}\n"));
+		user_str = utils::concat(&user_str, &format!("{id}\n"));
 	}
 	name_str = name_str.trim_end_matches('\n').to_string();
 	user_str = user_str.trim_end_matches('\n').to_string();
@@ -71,10 +54,10 @@ pub fn rolelist_embed(ctx: &serenity::Context, group: &str, rolelist: Vec<db::Ro
 
 pub fn rolelist_components(group: &str) -> serenity::CreateActionRow {
 	CreateActionRow::Buttons(vec![
-		CreateButton::new(concat("roles.pick.", group))
+		CreateButton::new(utils::concat("roles.pick.", group))
 			.label("Choose")
 			.style(serenity::ButtonStyle::Primary),
-		CreateButton::new(concat("roles.edit.", group))
+		CreateButton::new(utils::concat("roles.edit.", group))
 			.label("Modify")
 			.style(serenity::ButtonStyle::Danger)
 	])
